723f7688119f87ec2da768a23cd44a9b27170928
Paulo Freitas <42820569+paulofreitas-py@users.noreply.github.com>
2022-02-24 21:34:50 -0300

    DIO .Net

--- Digital/Program.cs
+++ Digital/Program.cs
@@ -8,5 +8,12 @@ namespace Digital
         {
             Console.WriteLine("Hello World!");
         }
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-Vindo ao Curso de .NET {i}");
+            }
+
     }
 }
